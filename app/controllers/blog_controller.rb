8fa6d21129c18aa6392689f23037d7acf94be301
sethcrist <sethcrist50@gmail.com>
2024-03-28 08:59:43 -0700

    added create method

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -6,4 +6,20 @@ class BlogController < ApplicationController
     def show 
         @blog = Blog.find(params[:id])
     end
+
+    def new
+        @blog = Blog.new
+    end
+
+    def create
+        @blog = Blog.create(blog_params)
+        if @blog.valid?
+            redirect_to blogs_path
+        end
+    end
+
+    private
+    def blog_params
+        params.require(:blog).permit(:title, :content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/blogs', to: 'blog#index', as: 'blogs'
+  get '/blogs/new', to: 'blog#new', as: 'blog_new'
   get '/blogs/:id', to: 'blog#show', as: 'blog'
+  post '/blogs', to: 'blog#create'
   root 'blog#index'
 end
